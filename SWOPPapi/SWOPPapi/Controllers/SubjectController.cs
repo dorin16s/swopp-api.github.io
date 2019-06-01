fb9909ebb53449c9b0410687ac2f2a78d4442359
dorin16s <dorin16s@gmail.com>
2019-06-01 18:19:52 +0300

    swopp

---
+++ SWOPPapi/SWOPPapi/Controllers/SubjectController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SWOPPapi.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using SWOPPapi.Models;
+
+    namespace TodoApi.Controllers
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+        public class TodoController : ControllerBase
+        {
+            private readonly subject _context;
+
+            public TodoController(subject context)
+            {
+
+            }
+
+            [HttpGet("{test}")]
+            public async Task<ActionResult<subject>> GetTodoItem(String test)
+            {
+                return null;
+            }
+
+        }
+    }
+}
---
+++ SWOPPapi/SWOPPapi/Models/subject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SWOPPapi.Models
+{
+    public class subject
+    {
+ 
+            public string Id { get; set; }
+            public int  hard { get; set; }
+            public bool easy { get; set; }
+  
+    }
+}
